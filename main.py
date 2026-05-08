31c7d0af85213db4b1cfe6530698441d15587623
20317daehee <kko77-s250212@gclass.ice.go.kr>
2026-05-08 15:44:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# main.py
 import streamlit as st
+import folium
+import strimlit_folium import st_folium
+import pandas as pd 
 
-st.text("안녕~~ 남동고등학교 여러분,, ")
+#1. 웹 페이지 설정
+st.set_page_config(page_title="남동고 등산메이트")
+st.text("안녕~~ 남동고등학교 여러분, 첫 페이지를 만드셧습니다.")
+
+
+
+st.text("안녕~~ 남동고등학교 여러분, 첫 페이지를 만드셧습니다.")
