8e8c964cc0f0816f0c146bbcff55b5fe1b1fcf24
20317daehee <kko77-s250212@gclass.ice.go.kr>
2026-05-08 15:52:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import strimlit_folium import st_folium
 import pandas as pd 
 
 #1. 웹 페이지 설정
-st.set_page_config(page_title="남동고 등산메이트",layout="wide")
+st.set_page_config(page_title="남동고 등산메이트", layout="wide")
 st.title("🏔️2026 학교 등산 행사 안내 지도")
+st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
+st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해보세요.")
+st.markdown("# 큰 제목")
+st.markdown("## 작은 제목")
+st.markdown("** 굵은 글씨**와*이탤릭체*")
 st.text("안녕~~ 남동고등학교 여러분, 첫 페이지를 만드셧습니다.")
 
 
